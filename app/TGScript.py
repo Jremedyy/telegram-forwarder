ca3534ed7c19dc20fb4d50be5e79db8712eb48cc
Jeremy Biagiotti <jbiagiotti8@gmail.com>
2024-12-02 06:36:53 -0500

    add docker file

--- app/TGScript.py
+++ app/TGScript.py
@@ -1,22 +1,28 @@
 import os
+import requests
+from telethon.sync import TelegramClient
 from dotenv import load_dotenv
 from telethon import TelegramClient, events
-import requests
+from telethon.sessions import StringSession
 
 # Load .env file
 load_dotenv()
 
 # Telegram API credentials from .env
+print(f"STRING_SESSION: {os.getenv('TELEGRAM_API_ID')}")
 API_ID = int(os.getenv("TELEGRAM_API_ID"))
 API_HASH = os.getenv("TELEGRAM_API_HASH")
-
+STRING_SESSION = os.getenv("STRING_SESSION_KEY")
 if not API_ID or not API_HASH:
     raise ValueError("Missing required environment variables.")
 
+
 print(f"BOOTING UP!")
 
+
+
 # Initialize Telegram client
-client = TelegramClient('session_name', API_ID, API_HASH)
+client = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
 
 # Mapping of Telegram channels to Discord webhooks
 channel_to_webhook = {
